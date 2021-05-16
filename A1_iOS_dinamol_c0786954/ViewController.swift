8686e95021d89ace3a454bca9a1a99eac8f55386
Amanpreetkaur <aman021011@gmail.com>
2021-05-16 02:16:17 -0400

    Initial Commit

---
+++ A1_iOS_dinamol_c0786954/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  A1_iOS_dinamol_c0786954
+//
+//  Created by MacStudent on 2021-05-16.
+//  Copyright © 2021 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
